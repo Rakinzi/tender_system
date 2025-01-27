7a134ebd2d689ada24050c9f9840715381bc5160
Rakinzi Silver <silverrakinzi@gmail.com>
2025-01-27 11:41:06 +0200

    Added Tender

---
+++ services/tender/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from ..models import Tender, Document, User, TenderTimeline
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ["document_type", "file", "description"]
+
+
+class TenderCreateSerializer(serializers.ModelSerializer):
+    documents = DocumentSerializer(many=True, rqeuired=False)
+    timeline = serializers.JSONField(required=False)
+
+    class Meta:
+        model = Tender
+        fields = [
+            "tender_name",
+            "description",
+            "reference_number",
+            "budget",
+            "deadline",
+            "category",
+            "required_department",
+            "company",
+            "documents",
+            "timeline",
+        ]
+
+    def create(self, validated_data):
+        documents_data = validated_data.pop("documents", [])
+        timeline_data = validated_data.pop("timeline", None)
+
+        tender = Tender.objects.create(**validated_data)
+
+        if timeline_data:
+            TenderTimeline.objects.create(tender=tender, **timeline_data)
+        else:
+            tender.get_timeline()
+
+        for doc_data in documents_data:
+            Document.objects.create(
+                tender=tender, uploader=validated_data["created_by"], **doc_data
+            )
+
+        managers = User.objects.filter(
+            department=tender.required_department,
+            role="manager",
+            company=tender.company,
+        )
+        tender.assigned_to.add(*managers)
+        return tender
